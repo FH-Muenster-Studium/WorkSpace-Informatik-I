cd2e735febc5996cfc8e23cb24c204ed72239b17
fabianterhorst <fabian.terhorst@gmail.com>
2016-11-02 13:48:27 +0100

    Improve doc and small cleanup

--- Praktikum1/src/quadratwurzel.c
+++ Praktikum1/src/quadratwurzel.c
@@ -6,14 +6,14 @@
  * Aufrufer übergibt und gibt das Ergebnis an den Aufrufer zurück.
  */
 double wurzel(int zahl) {
-    return sqrt(zahl);
+	return sqrt(zahl);
 }
 
 /* Diese Funktion berechnet das Quadrat der Zahl, die der
  * Aufrufer übergibt und gibt das Ergebnis an den Aufrufer zurück.
  */
 double quadrat(int zahl) {
-    return pow(zahl, 2);
+	return pow(zahl, 2);
 }
 
 /*
@@ -24,33 +24,34 @@ double kreis_flaeche(int zahl) {
 }
 
 int main(int argc, char* argv[]) {
-    char puffer[32];
-    int eingabe;
-    double ergebnis;
+	char puffer[32];
+	int eingabe;
+	double ergebnis;
 
-    /* Hier muss der Benutzer etwas eingeben. */
-    puts("Bitte geben Sie eine Zahl ein: ");
-    fflush(stdout);
-    fgets(puffer, 32, stdin);
-    eingabe = atoi(puffer);
+	/* Hier muss der Benutzer etwas eingeben. */
+	puts("Bitte geben Sie eine Zahl ein: ");
+	fflush(stdout);
+	fgets(puffer, 32, stdin);
+	eingabe = atoi(puffer);
 
-    /* Ruf die Funktion 'wurzel' auf und speichere das Ergebnis */
-    ergebnis = wurzel(eingabe);
-    printf("Die Quadratwurzel der Zahl %d ist %f\n", eingabe, ergebnis);
+	/* Ruf die Funktion 'wurzel' auf und speichere das Ergebnis */
+	ergebnis = wurzel(eingabe);
+	printf("Die Quadratwurzel der Zahl %d ist %f\n", eingabe, ergebnis);
 
 
-    /* Ruf die Funktion 'quadrat' auf und speichere das Ergebnis */
-    ergebnis = quadrat(eingabe);
-    printf("Das Quadrat der Zahl %d ist %f\n", eingabe, ergebnis);
+	/* Ruf die Funktion 'quadrat' auf und speichere das Ergebnis */
+	ergebnis = quadrat(eingabe);
+	printf("Das Quadrat der Zahl %d ist %f\n", eingabe, ergebnis);
 
-    /* Hier wird gerechnet */
-    int r;
-    double f;
-    puts("| Radius | Kreisfläche");
-    for(r = 1; r <= 20; r++) {
-    	f = kreis_flaeche(r);
-    	printf("| %d \t | %f \n", r , f);
-    }
+	/* Ausgabe der Tabellenspalten Definitionen  */
+	puts("| Radius | Kreisfläche");
+	/* Iterieren der einzelnen Rechnungen mit den Wert x > 1 && x < 21*/
+	for(int i = 1; i < 21; i++) {
+		/* Die Kreisfläche wird anhand des Radius berechnet */
+		double f = kreis_flaeche(i);
+		/* Ausgabe der ausgerechneten Kreisfläche und des genutzten Radiuses */
+		printf("| %d \t | %f \n", i , f);
+	}
 
-    return 0;
+	return 0;
 }
